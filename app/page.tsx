030076503dfb85c7acbd348ae3d6817805648c45
syed minam ur rehman <138588241+minamrahman727@users.noreply.github.com>
2023-12-28 21:40:17 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ export default function Home() {
   return (
     <div className="flex justify-center">
         <h1 className=" font-serif text-4xl text-red-600 bg-gray-400 rounded-xl">Minam Group of Companies</h1>
+      </div>
+    <div>
       <Image
         src="logo.png" // Path to your image from the public folder
         alt="Description of your image"
