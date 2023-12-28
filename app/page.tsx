7dfe7418ecc18b0100ebf7906d58e12f1f43dff2
syed minam ur rehman <138588241+minamrahman727@users.noreply.github.com>
2023-12-28 20:57:58 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div>
-    <h1 className='font-serif text-2xl text-black bg-gray-500 rounded-3xl>minam</h1>
+        <h1 className=" font-serif text-3xl text-black bg-gray-600 rounded-3xl">minam</h1>
     </div>
   )
 }
