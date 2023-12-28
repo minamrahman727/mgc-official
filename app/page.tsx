ef5f9bad4ffbe84c1c8c4cd8ab5031f8fbdd2cec
syed minam ur rehman <138588241+minamrahman727@users.noreply.github.com>
2023-12-28 20:52:27 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,11 @@ export default function Home() {
   return (
     <div>
     <ul className="grid grid-cols-12 grid-rows-6h-screen border-spacing-6">
-      <li className="colspan-1 rowspan-5 bg-black text-white">minam</li>
+      <li className="colspan-1 rowspan-5 bg-black text-2xl text-white">minam</li>
     </ul>
+      <div className=' center-slider'>
+        <img src="/logo.png" alt="Slide 1" height={400} width={500} />
+      </div>
     </div>
   )
 }
