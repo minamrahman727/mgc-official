8b734736047f1a12176d17d2d760f81ccf091b5b
syed minam ur rehman <138588241+minamrahman727@users.noreply.github.com>
2023-12-28 22:00:05 +0500

    wow!page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,14 @@ export default function Home() {
       /> <h1 className=" font-serif text-4xl text-blue-600 bg-gray-200 border border-blue-300 rounded-xl">Minam Group of Companies</h1>
     </div>
      <div className="flex justify-center">
-     <h1 className="font-mono text-6xl text-red-600 border border-red-500 rounded-3xl">site under construction!</h1></div>
+     <h1 className="font-mono text-6xl text-red-600 border border-red-500 rounded-3xl">site under construction!</h1>
+      <Image
+        src="pic.png" // Path to your image from the public folder
+        alt="Description of your image"
+        width={700} // Set the desired width
+        height={700} // Set the desired height
+      />
+     </div>
      </div>
   )
 }
