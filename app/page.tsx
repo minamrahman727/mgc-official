fb0dfeed819d5d422a10d0b11dce77d493b88e25
syed minam ur rehman <138588241+minamrahman727@users.noreply.github.com>
2023-12-28 21:21:19 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div className="flex justify-center">
-        <h1 className=" font-serif text-3xl text-black bg-gray-600 rounded-3xl">Minam Group of Companies</h1>
+        <h1 className=" font-serif text-4xl text-red-600 bg-gray-400 rounded-3xl">Minam Group of Companies</h1>
     </div>
   )
 }
