d58ddc6884d422babaea66d94f26460d0a7107af
syed minam ur rehman <138588241+minamrahman727@users.noreply.github.com>
2023-12-28 21:43:35 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,13 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="flex justify-center">
-        <h1 className=" font-serif text-4xl text-red-600 bg-gray-400 rounded-xl">Minam Group of Companies</h1>
+    <div className="flex justify-evenly">
       <Image
         src="logo.png" // Path to your image from the public folder
         alt="Description of your image"
         width={200} // Set the desired width
         height={200} // Set the desired height
-      />
+      /> <h1 className=" font-serif text-4xl text-red-600 bg-gray-400 rounded-xl">Minam Group of Companies</h1>
     </div>
   )
 }
