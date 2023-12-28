266b113bbdfe031b3f6e2d74266eb20b415c3a30
syed minam ur rehman <138588241+minamrahman727@users.noreply.github.com>
2023-12-28 21:41:12 +0500

    now page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,11 @@ export default function Home() {
   return (
     <div className="flex justify-center">
         <h1 className=" font-serif text-4xl text-red-600 bg-gray-400 rounded-xl">Minam Group of Companies</h1>
-      </div>
-    <div>
       <Image
         src="logo.png" // Path to your image from the public folder
         alt="Description of your image"
-        width={500} // Set the desired width
-        height={300} // Set the desired height
+        width={200} // Set the desired width
+        height={200} // Set the desired height
       />
     </div>
   )
