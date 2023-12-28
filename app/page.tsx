d71b7cea4aa8841a39bbc93d5319eb500944ff67
syed minam ur rehman <138588241+minamrahman727@users.noreply.github.com>
2023-12-28 20:54:57 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div>
-    <ul className="grid grid-cols-12 grid-rows-6h-screen border-spacing-6">
-      <li className="colspan-1 rowspan-5 bg-black text-2xl text-white">minam</li>
-    </ul>
-      <div className=' center-slider'>
-        <img src="/logo.png" alt="Slide 1" height={400} width={500} />
-      </div>
+    <h1 className='font-serif text-2xl text-black bg-gray-500 rounded-3xl>minam</h1>
     </div>
   )
 }
