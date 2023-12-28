229ce3fed9cd55c2ba6d00a2d73b70111edd075f
syed minam ur rehman <138588241+minamrahman727@users.noreply.github.com>
2023-12-28 22:01:51 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       /> <h1 className=" font-serif text-4xl text-blue-600 bg-gray-200 border border-blue-300 rounded-xl">Minam Group of Companies</h1>
     </div>
      <div className="flex justify-center">
-     <h1 className="font-mono text-6xl text-red-600 border border-red-500 rounded-3xl">site under construction!</h1>
+     <h1 className="font-mono text-6xl text-red-600 ">site under construction!</h1>
       <Image
         src="pic.png" // Path to your image from the public folder
         alt="Description of your image"
