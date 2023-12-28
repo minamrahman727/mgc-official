e07d0ab73ae0322cbb7d3213d5cb2c0c94f68097
syed minam ur rehman <138588241+minamrahman727@users.noreply.github.com>
2023-12-28 21:35:01 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,12 @@ export default function Home() {
   return (
     <div className="flex justify-center">
         <h1 className=" font-serif text-4xl text-red-600 bg-gray-400 rounded-xl">Minam Group of Companies</h1>
+      <Image
+        src="logo.png" // Path to your image from the public folder
+        alt="Description of your image"
+        width={500} // Set the desired width
+        height={300} // Set the desired height
+      />
     </div>
   )
 }
